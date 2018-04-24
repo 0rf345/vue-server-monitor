cc183bb5a0b1163645d0370cbfa53e370ac22bdd
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2018-04-24 19:32:26 +0300

    Battling with importing anything in a test file

---
+++ src/components/MainView.spec.js
@@ -0,0 +1,10 @@
+import add from './testable'
+
+describe('MainView.vue', () => {
+  test('setup correctly', () => {
+    expect(true).toBe(true)
+  })
+  test('sanity test', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
--- src/tests/sandbox.test.js
+++
@@ -1,3 +0,0 @@
-it('Nothing', () =>
-  expect(1).toBe(1)
-)
