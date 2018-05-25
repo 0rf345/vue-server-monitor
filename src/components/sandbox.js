9765cda85ef793b7fad3e5ee12bf5b9e692740ee
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2018-05-25 10:13:40 +0300

    Removed deprecated code

--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -4,8 +4,6 @@ let apiKey = 'u587507-ef028e160c997d362add8741'
 let url = 'https://api.uptimerobot.com/v2/getMonitors'
 var limit = 10
 var offset = 0
-var monitors = []
-var globalArr = []
 var total = 0
 var timeOUTms = 10 * 1000
 
@@ -26,7 +24,7 @@ let apiPOSTrequest = (localLimit, localOffset) => {
 }
 
 let paginationMaster = (() => {
-  axios.all([apiPOSTrequest(limit, offset)])
+  return axios.all([apiPOSTrequest(limit, offset)])
     .then((res0) => {
       let firstMonitors = res0[0].data.monitors
       let pagination = res0[0].data.pagination
@@ -40,32 +38,15 @@ let paginationMaster = (() => {
         apiCallArr.push(apiPOSTrequest(limit, offset))
       }
 
-      axios.all(apiCallArr).then((resArray) => {
+      return axios.all(apiCallArr).then((resArray) => {
         let resMonitors = firstMonitors.slice()
         for (let index in resArray) {
-          resMonitors.concat(resArray[index].data.monitors)
+          resMonitors = resMonitors.concat(resArray[index].data.monitors)
         }
-        console.log(resMonitors.length)
+        return resMonitors
       })
     })
 })
 
 paginationMaster()
-
-/*
-axios.all(
-  (() => {
-    let i = 0
-    let res = []
-    for (i; i < 3; i++) {
-      res.push(a(i))
-    }
-    return res
-  })()
-)
-  .then(((resArray) => {
-    console.log(resArray[0].data.monitors.concat(resArray[1].data.monitors.concat(resArray[2].data.monitors)).length)
-  }))
-  */
-
-//  goGetter().then(res => console.log(res.length))
+  .then(res => console.log(res.length))
